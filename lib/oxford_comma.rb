1116db7f2bc000226b9b2cf9289bdcfd4a305eb3
katherineyuneman <kyuneman@gmail.com>
2022-03-12 12:41:07 -0500

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+    length=array.length
+    case length
+    when length = 1
+        array.join()
+    when length = 2
+        new_last_index = " " + "and" + " " + array[array.length - 1]
+        array[array.length-1] = new_last_index
+        array.join()
+    else
+        array[array.length - 1] = "and" + " " + array[array.length - 1]
+        array.join(", ")
+    end
 end
+
+
+    # array.length > 1 ?
+    # array[array.length - 1] = "and" + " " + array[array.length - 1] : 
+    # array.join(",")
+    # # new_last_index = "and" + " " + array[array.length - 1]
+    # # array[array.length - 1] = new_last_index
+    # # array.join(",")
+
+
+# oxford_comma(["fiddleheads", "okra", "kohlrabi"])
